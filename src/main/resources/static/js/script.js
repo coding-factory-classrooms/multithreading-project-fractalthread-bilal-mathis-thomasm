ded90c8ae0da4f761cc0bfe812d44c5a159ce61d
Mathis <mathis.timba@gmail.com>
2021-01-12 09:21:18 +0100

    Auto stash before merge of "mathis" and "origin/master"

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,6 +2,8 @@ const fractalDiv = document.getElementById("fractal-img");
 const step = 5;
 const allPos = {x:0,y:0,z:3}
 
+updateImage()
+
 function move(axe, increment){
     increment *= (Math.log(2) * allPos.z) / step;
     allPos[axe] += increment;
